4b1ea79261c93e9b538b187132ad0210e220b08e
Nishant Srivastava <nisrulz@gmail.com>
2017-03-08 11:10:53 -0800

    code refactor, updated images, updated layout

--- public/js/main.js
+++ public/js/main.js
@@ -1,44 +1,20 @@
-function escapeHtml (string) {
-  var entityMap = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&#39;',
-    '/': '&#x2F;'
-  }
-  return String(string).replace(/[&<>"'\/]/g, function (s) {
-    return entityMap[s]
-  })
-}
-
-function generateHTML () {
-  var codeblock = document.getElementById('preview')
-  var head = '<!DOCTYPE html><html> <head> <meta charset="utf-8"> <meta name="viewport" content="width=device-width"> <title>Privacy Policy</title> <style>body{font-family: "Helvetica Neue", Helvetica, Arial, sans-serif; padding:1em;}</style></head> <body>'
-  var end = '</body></html>'
-  var html = head + codeblock.innerHTML + end
-  html = escapeHtml(html)
-  document.getElementById('html').innerHTML = html
-}
-
 var vueapp = new Vue({
   el: '#vueapp',
   data: {
     iOrWe: '[I | we]',
-    typeOfApp: '[open source | free | freemium | ad-supported | commercial]',
-    typeOfDev: '[Individual or Company]',
+    typeOfApp: '',
+    typeOfAppTxt: '[open source | free | freemium | ad-supported | commercial]',
+    typeOfDev: '',
     appName: '[App Name]',
     myOrOur: '[my|our]',
     meOrUs: '[me|us]',
-    pidInfo: '[add whatever else you collect here, e.g. users name | address | location | pictures]',
     atNoCost: '[at no cost]',
-    devOrCompanyName: '[Dev Name or Company Name]',
     retainedInfo: '[retained on your device and is not collected by [me|us] in any way]|[will be retained by us and used as described in this privacy policy].',
     devName: '',
     companyName: '',
-    devOrCompanyName: '',
+    devOrCompanyName: '[Developer name or Company name]',
     pidInfoIn: '',
-    typeOfAppTxt: ''
+    pidInfo: '[add whatever else you collect here, e.g. users name | address | location | pictures]'
   },
   methods: {
     generate: function () {
@@ -84,3 +60,26 @@ var vueapp = new Vue({
     }
   }
 })
+
+function escapeHtml (string) {
+  var entityMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;',
+    '/': '&#x2F;'
+  }
+  return String(string).replace(/[&<>"'\/]/g, function (s) {
+    return entityMap[s]
+  })
+}
+
+function generateHTML () {
+  var codeblock = document.getElementById('preview')
+  var head = '<!DOCTYPE html><html> <head> <meta charset="utf-8"> <meta name="viewport" content="width=device-width"> <title>Privacy Policy</title> <style>body{font-family: "Helvetica Neue", Helvetica, Arial, sans-serif; padding:1em;}</style></head> <body>'
+  var end = '</body></html>'
+  var html = head + codeblock.innerHTML + end
+  html = escapeHtml(html)
+  document.getElementById('html').innerHTML = html
+}
