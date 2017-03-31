f283b580022163d212ef25c4fc37f68f7d64035e
Nishant Srivastava <nisrulz@gmail.com>
2017-03-30 23:56:50 -0700

    wip - replace bootstrap with mdl

--- public/js/main.js
+++ public/js/main.js
@@ -14,17 +14,28 @@ var vueapp = new Vue({
     companyName: '',
     devOrCompanyName: '[Developer name or Company name]',
     pidInfoIn: '',
-    pidInfo: '[add whatever else you collect here, e.g. users name | address | location | pictures]'
+    pidInfo: '[add whatever else you collect here, e.g. users name | address | location | pictures]',
+    gps: true,
+    admob: false
   },
   methods: {
+    selectAllText: function () {
+      selectText('privacy_content')
+    },
+    generateHTML: function () {
+      let content = getContent()
+      let rawHTML = getRawHTML(content)
+      let filename = 'privacy_policy'
+      downloadHTML(filename, rawHTML)
+    },
     generate: function () {
-      if (this.typeOfDev == 'Individual') {
+      if (this.typeOfDev === 'Individual') {
         this.devOrCompanyName = this.devName
         this.iOrWe = 'I'
         this.myOrOur = 'my'
         this.meOrUs = 'me'
         this.retainedInfo = 'retained on your device and is not collected by ' + this.meOrUs + ' in any way'
-      }else if (this.typeOfDev == 'Company') {
+      } else if (this.typeOfDev === 'Company') {
         this.devOrCompanyName = this.companyName
         this.iOrWe = 'we'
         this.myOrOur = 'our'
@@ -32,9 +43,9 @@ var vueapp = new Vue({
         this.retainedInfo = 'will be retained by us and used as described in this privacy policy.'
       }
 
-      if (this.typeOfApp == 'Commercial') {
+      if (this.typeOfApp === 'Commercial') {
         this.atNoCost = ''
-      }else {
+      } else {
         this.atNoCost = 'at no cost'
       }
 
@@ -55,31 +66,52 @@ var vueapp = new Vue({
           this.typeOfAppTxt = 'an ' + this.typeOfApp
           break
       }
-
-      generateHTML()
+      window.location.href = '#privacypolicy'
     }
   }
 })
 
-function escapeHtml (string) {
-  var entityMap = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&#39;',
-    '/': '&#x2F;'
-  }
-  return String(string).replace(/[&<>"'\/]/g, function (s) {
-    return entityMap[s]
-  })
-}
-
-function generateHTML () {
-  var codeblock = document.getElementById('preview')
+function getRawHTML (content) {
   var head = '<!DOCTYPE html><html> <head> <meta charset="utf-8"> <meta name="viewport" content="width=device-width"> <title>Privacy Policy</title> <style>body{font-family: "Helvetica Neue", Helvetica, Arial, sans-serif; padding:1em;}</style></head> <body>'
   var end = '</body></html>'
-  var html = head + codeblock.innerHTML + end
-  html = escapeHtml(html)
-  document.getElementById('html').innerHTML = html
+  var html = head + content + end
+  return html
+}
+
+function getContent () {
+  var content = document.getElementById('privacy_content')
+  return content.innerHTML
+}
+
+function selectText (containerId) {
+  var range
+  if (document.selection) {
+    range = document.body.createTextRange()
+    range.moveToElementText(document.getElementById(containerId))
+    range.select()
+  } else if (window.getSelection) {
+    range = document.createRange()
+    range.selectNode(document.getElementById(containerId))
+    window.getSelection().addRange(range)
+  }
+}
+
+function download (filename, text, format) {
+  format += ';charset=utf-8,'
+  var pom = document.createElement('a')
+  pom.setAttribute('href', format + encodeURIComponent(text))
+  pom.setAttribute('download', filename)
+
+  if (document.createEvent) {
+    var event = document.createEvent('MouseEvents')
+    event.initEvent('click', true, true)
+    pom.dispatchEvent(event)
+  } else {
+    pom.click()
+  }
+}
+
+function downloadHTML (filename, content) {
+  filename += '.html'
+  download(filename, content, 'data:text/html')
 }
