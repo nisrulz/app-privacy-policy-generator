a1951e3e986e47da099b5962ea95679c9b942b11
Chris Beauchamp <cjbeauchamp@gmail.com>
2019-04-19 10:42:30 -0400

    flycricket link

---
+++ public/js/flycricket.js
@@ -0,0 +1,53 @@
+/*!
+ * Serialize all form data into a query string
+ * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {Node}   form The form to serialize
+ * @return {String}      The serialized form data
+ */
+var serialize = function (form) {
+
+	// Setup our serialized data
+	var serialized = [];
+
+	// Loop through each field in the form
+	for (var i = 0; i < form.elements.length; i++) {
+
+		var field = form.elements[i];
+
+		// Don't serialize fields without a name, submits, buttons, file and reset inputs, and disabled fields
+		if (!field.name || field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') continue;
+
+		// If a multi-select, get all selections
+		if (field.type === 'select-multiple') {
+			for (var n = 0; n < field.options.length; n++) {
+				if (!field.options[n].selected) continue;
+				serialized.push(encodeURIComponent(field.name) + "=" + encodeURIComponent(field.options[n].value));
+			}
+		}
+
+		// Convert field data to a query string
+		else if ((field.type !== 'checkbox' && field.type !== 'radio') || field.checked) {
+			serialized.push(encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value));
+		}
+	}
+
+	return serialized.join('&');
+
+};
+
+function fc_submit(e) {
+	// e.preventDefault();
+
+	// fill in the body with the html content
+	document.getElementById("fc-body").value = getContent('privacy_content');
+
+	var form = document.querySelector('#fc-form');
+	var formData = serialize(form);
+
+	console.log(formData);
+	
+	// fill the fields with our JS
+	
+	return true;
+}
+
