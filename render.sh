d0aa1712c5c9ae0b2de00360819f9c372de358e6
Nishant Srivastava <nisrulz@gmail.com>
2020-09-19 21:07:02 +0200

    setup sass for handling css partials

--- render.sh
+++ render.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 # Using pug-cli to render from pug to html
-# Install pug-cli:npm install -g pug-cli
+# Install pug-cli: npm install -g pug-cli
 pug src/index.pug --pretty --out public
+
+# Using sass-cli to render from sass to css
+# Install sass-cli: npm install -g sass
+sass src/sass/style.sass public/css/style.css
