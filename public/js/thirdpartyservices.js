8cc4c84c8b5e6fdcb2c34076b0988115a38f0ba0
Nishant Srivastava <nisrulz@gmail.com>
2021-04-07 09:05:31 +0200

    updated rendered code

--- public/js/thirdpartyservices.js
+++ public/js/thirdpartyservices.js
@@ -23,7 +23,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Google Play Services",
     "model": "gps",
     "gps": true,
-    "logo": "images/gps.png",
+    "logo": "images/third_party_logos/gps.png",
     "link": {
       "privacy": "https://www.google.com/policies/privacy/",
       "terms": "https://policies.google.com/terms"
@@ -33,7 +33,7 @@ var thirdPartyServicesJsonArray = [
     "name": "AdMob",
     "model": "admob",
     "admob": false,
-    "logo": "images/admob.png",
+    "logo": "images/third_party_logos/admob.png",
     "link": {
       "privacy": "https://support.google.com/admob/answer/6128543?hl=en",
       "terms": "https://developers.google.com/admob/terms"
@@ -43,7 +43,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Google Analytics for Firebase",
     "model": "firebase",
     "firebase": false,
-    "logo": "images/firebase.png",
+    "logo": "images/third_party_logos/firebase.png",
     "link": {
       "privacy": "https://firebase.google.com/policies/analytics",
       "terms": "https://firebase.google.com/terms/analytics"
@@ -53,7 +53,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Firebase Crashlytics",
     "model": "crashlytics",
     "crashlytics": false,
-    "logo": "images/firebase.png",
+    "logo": "images/third_party_logos/firebase.png",
     "link": {
       "privacy": "https://firebase.google.com/support/privacy/",
       "terms": "https://firebase.google.com/terms/crashlytics"
@@ -63,7 +63,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Facebook",
     "model": "facebook",
     "facebook": false,
-    "logo": "images/facebook.png",
+    "logo": "images/third_party_logos/facebook.png",
     "link": {
       "privacy": "https://www.facebook.com/about/privacy/update/printable",
       "terms": "https://www.facebook.com/legal/terms/plain_text_terms"
@@ -73,7 +73,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Fabric",
     "model": "fabric",
     "fabric": false,
-    "logo": "images/fabric.png",
+    "logo": "images/third_party_logos/fabric.png",
     "link": {
       "privacy": "https://policies.google.com/privacy",
       "terms": "https://fabric.io/terms/fabric"
@@ -83,7 +83,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Matomo",
     "model": "piwik",
     "piwik": false,
-    "logo": "images/piwik.png",
+    "logo": "images/third_party_logos/piwik.png",
     "link": {
       "privacy": "https://matomo.org/privacy-policy/",
       "terms": "https://matomo.org/terms"
@@ -93,7 +93,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Clicky",
     "model": "clicky",
     "clicky": false,
-    "logo": "images/clicky.png",
+    "logo": "images/third_party_logos/clicky.png",
     "link": {
       "privacy": "https://clicky.com/terms#privacy",
       "terms": "https://clicky.com/terms/"
@@ -103,7 +103,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Flurry Analytics",
     "model": "flurry",
     "flurry": false,
-    "logo": "images/flurry.png",
+    "logo": "images/third_party_logos/flurry.png",
     "link": {
       "privacy": "https://privacy.oath.com",
       "terms": "https://developer.yahoo.com/flurry/legal-privacy/terms-service/flurry-analytics-terms-service.html"
@@ -113,7 +113,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Appodeal",
     "model": "appodeal",
     "appodeal": false,
-    "logo": "images/appodeal.png",
+    "logo": "images/third_party_logos/appodeal.png",
     "link": {
       "privacy": "https://www.appodeal.com/home/privacy-policy/",
       "terms": "https://www.appodeal.com/home/terms-of-service/"
@@ -123,7 +123,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Fathom Analytics",
     "model": "fathom",
     "fathom": false,
-    "logo": "images/fathom.png",
+    "logo": "images/third_party_logos/fathom.png",
     "link": {
       "privacy": "https://usefathom.com/privacy/",
       "terms": "https://usefathom.com/terms/"
@@ -133,7 +133,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Unity",
     "model": "unity",
     "unity": false,
-    "logo": "images/unity.png",
+    "logo": "images/third_party_logos/unity.png",
     "link": {
       "privacy": "https://unity3d.com/legal/privacy-policy",
       "terms": "https://unity3d.com/legal/terms-of-service"
@@ -143,7 +143,7 @@ var thirdPartyServicesJsonArray = [
     "name": "SDKBOX",
     "model": "sdkbox",
     "sdkbox": false,
-    "logo": "images/sdkbox.png",
+    "logo": "images/third_party_logos/sdkbox.png",
     "link": {
       "privacy": "https://www.sdkbox.com/privacy",
       "terms": "https://www.sdkbox.com/privacy"
@@ -153,7 +153,7 @@ var thirdPartyServicesJsonArray = [
     "name": "GameAnalytics",
     "model": "gameanalytics",
     "gameanalytics": false,
-    "logo": "images/gameanalytics.png",
+    "logo": "images/third_party_logos/gameanalytics.png",
     "link": {
       "privacy": "https://gameanalytics.com/privacy",
       "terms": "https://gameanalytics.com/terms"
@@ -163,7 +163,7 @@ var thirdPartyServicesJsonArray = [
     "name": "One Signal",
     "model": "onesignal",
     "onesignal": false,
-    "logo": "images/one_signal.svg",
+    "logo": "images/third_party_logos/one_signal.svg",
     "link": {
       "privacy": "https://onesignal.com/privacy_policy",
       "terms": "https://onesignal.com/tos"
@@ -173,7 +173,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Expo",
     "model": "expo",
     "expo": false,
-    "logo": "images/expo.png",
+    "logo": "images/third_party_logos/expo.png",
     "link": {
       "privacy": "https://expo.io/privacy",
       "terms": "https://expo.io/terms"
@@ -183,7 +183,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Sentry",
     "model": "sentry",
     "sentry": false,
-    "logo": "images/sentry.png",
+    "logo": "images/third_party_logos/sentry.png",
     "link": {
       "privacy": "https://sentry.io/privacy/",
       "terms": "https://sentry.io/terms/"
@@ -193,7 +193,7 @@ var thirdPartyServicesJsonArray = [
     "name": "AppLovin",
     "model": "applovin",
     "applovin": false,
-    "logo": "images/applovin.png",
+    "logo": "images/third_party_logos/applovin.png",
     "link": {
       "privacy": "https://www.applovin.com/privacy/",
       "terms": "https://www.applovin.com/terms/"
@@ -203,7 +203,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Vungle",
     "model": "vungle",
     "vungle": false,
-    "logo": "images/vungle.png",
+    "logo": "images/third_party_logos/vungle.png",
     "link": {
       "privacy": "https://vungle.com/privacy/"
     }
@@ -212,7 +212,7 @@ var thirdPartyServicesJsonArray = [
     "name": "StartApp",
     "model": "startapp",
     "startapp": false,
-    "logo": "images/startapp.png",
+    "logo": "images/third_party_logos/startapp.png",
     "link": {
       "privacy": "https://www.startapp.com/privacy/",
       "terms": "https://www.startapp.com/policy/publisher-terms/"
@@ -222,7 +222,7 @@ var thirdPartyServicesJsonArray = [
     "name": "AdColony",
     "model": "adcolony",
     "adcolony": false,
-    "logo": "images/adcolony.png",
+    "logo": "images/third_party_logos/adcolony.png",
     "link": {
       "privacy": "https://www.adcolony.com/privacy-policy/"
     }
@@ -231,7 +231,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Amplitude",
     "model": "amplitude",
     "amplitude": false,
-    "logo": "images/amplitude.svg",
+    "logo": "images/third_party_logos/amplitude.svg",
     "link": {
       "privacy": "https://amplitude.com/privacy",
       "terms": "https://amplitude.com/terms"
@@ -241,7 +241,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Adjust",
     "model": "adjust",
     "adjust": false,
-    "logo": "images/adjust.png",
+    "logo": "images/third_party_logos/adjust.png",
     "link": {
       "privacy": "https://www.adjust.com/terms/privacy-policy",
       "terms": "https://www.adjust.com/terms/general-terms-and-conditions"
@@ -251,7 +251,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Mapbox",
     "model": "mapbox",
     "mapbox": false,
-    "logo": "images/mapbox.png",
+    "logo": "images/third_party_logos/mapbox.png",
     "link": {
       "privacy": "https://www.mapbox.com/legal/privacy",
       "terms": "https://www.mapbox.com/legal/tos"
@@ -261,7 +261,7 @@ var thirdPartyServicesJsonArray = [
     "name": "Godot",
     "model": "godot",
     "godot": false,
-    "logo": "images/godot.png",
+    "logo": "images/third_party_logos/godot.png",
     "link": {
       "privacy": "https://godotengine.org/privacy-policy",
       "terms": "https://godotengine.org/license"
@@ -271,10 +271,20 @@ var thirdPartyServicesJsonArray = [
     "name": "Segment",
     "model": "segment",
     "sentry": false,
-    "logo": "images/segment.png",
+    "logo": "images/third_party_logos/segment.png",
     "link": {
       "privacy": "https://segment.com/legal/privacy/",
       "terms": "https://segment.com/legal/terms/"
+    }
+  },
+  {
+    "name": "Mixpanel",
+    "model": "mixpanel",
+    "mixpanel": false,
+    "logo": "images/third_party_logos/mixpanel.png",
+    "link": {
+      "privacy": "https://mixpanel.com/legal/privacy-policy/",
+      "terms": "https://mixpanel.com/legal/terms-of-use/"
     }
   }
 ]
