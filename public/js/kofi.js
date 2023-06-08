2153cca233d17ef90776c8ee9c3f4e9bd02c443d
Nishant Srivastava <nisrulz@gmail.com>
2023-06-08 02:21:12 +0200

    Setup kofi widget

---
+++ public/js/kofi.js
@@ -0,0 +1,6 @@
+kofiWidgetOverlay.draw("nisrulz", {
+  type: "floating-chat",
+  "floating-chat.donateButton.text": "Support me",
+  "floating-chat.donateButton.background-color": "#fcbf47",
+  "floating-chat.donateButton.text-color": "#323842",
+});
