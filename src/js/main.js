6634db9e35f515ba856e2ac44a104ec0f4eaa710
Nishant Srivastava <nisrulz@users.noreply.github.com>
2025-12-04 23:55:08 +0100

    Add AI Usage section + update review page (#302)
    * add option to add section for AI usage.
    Fixes #293
    
    * updated reviews page
    
    * generate files

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,21 @@
 /*  
-	App Privacy Policy Generator: A simple web app to generate a generic 
-	privacy policy for your Android/iOS apps
+  App Privacy Policy Generator: A simple web app to generate a generic 
+  privacy policy for your Android/iOS apps
 
-	Copyright 2017-Present Nishant Srivastava
+  Copyright 2017-Present Nishant Srivastava
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 const { createApp } = Vue;
@@ -56,6 +56,7 @@ const app = createApp({
       typeOfPolicy: "Simple",
       typeOfPolicyInt: 1,
       isLocationTracked: false,
+      isAIUsed: false,
     };
   },
 
